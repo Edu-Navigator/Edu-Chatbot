ff3c16df6ca02e15eae0d681cd84d489c8d23475
sojeong <sosoj1552@gmail.com>
2025-12-24 15:43:44 +0900

    feat: 우리동네 디지털 안내소의 정보 업데이트 체크 DAG 추가
    - 업데이트를 확인하고 메일발송하는 기능

---
+++ airflow/dags/urdn_freshness_check.py
@@ -0,0 +1,41 @@
+from airflow import DAG
+import pendulum
+
+from common.default_args import DEFAULT_ARGS
+from airflow.operators.email import EmailOperator
+from airflow.operators.empty import EmptyOperator
+
+from scripts.urdn_email_task import (
+    crawl_update_date, 
+    get_max_created_at, 
+    compare_dates, 
+    URDN_UPDATE_ALERT_TEMPLATE
+)
+
+with DAG(
+    dag_id = '00_check_urdn_center_update',
+    start_date=pendulum.datetime(2025, 12, 1, 0, 0, 
+                                tz=pendulum.timezone("Asia/Seoul")), 
+    schedule="30 9 * * *", # start_date의 tz 기준 오전 9시 30분 실행
+    catchup = False,
+    tags=['00', 'check', 'email', 'urdn'],
+    default_args=DEFAULT_ARGS,
+) as dag :
+
+    update_cycle = crawl_update_date()
+    max_created = get_max_created_at()
+    
+    branch = compare_dates(update_cycle, max_created)
+    send_email = EmailOperator(
+        task_id='send_email',
+        to=DEFAULT_ARGS['email'],
+        subject='[Alert] 우리동네 디지털 안내소 데이터 업데이트 필요',
+        html_content=URDN_UPDATE_ALERT_TEMPLATE
+    )
+    
+    # 5. 종료
+    end_task = EmptyOperator(
+        task_id='end_task',
+    )
+    
+    branch >> [send_email, end_task]
